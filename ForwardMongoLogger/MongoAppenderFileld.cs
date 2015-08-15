39f61bf99730d762464edb5e28894b2e50b5a496
AzharIqbal84 <azhar.iqbal@aol.com>
2015-08-15 14:54:29 -0700

    added project impl

---
+++ ForwardMongoLogger/MongoAppenderFileld.cs
@@ -0,0 +1,11 @@
+﻿
+using log4net.Layout;
+
+namespace ForwardMongoLogger
+{
+    public class MongoAppenderFileld
+    {
+        public string Name { get; set; }
+        public IRawLayout Layout { get; set; }
+    }
+}
---
+++ Test/ForwardMongoLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ForwardMongoLogger
+    {
+        [TestMethod]
+        public void Log_Info_Message()
+        {
+            XmlConfigurator.Configure();
+            ILog log = LogManager.GetLogger(typeof(ForwardMongoLogger));
+
+            log.Info("some info ");
+        }
+    }
+}
